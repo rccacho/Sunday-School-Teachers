557c0f018d68969238cecf50d92f67a4ae516af9
rccacho <rccacho@gmail.com>
2017-04-08 21:21:01 -0700

    generate User scaffold and migrated

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+    last_name "MyString"
+    email "MyString"
+    password "MyString"
+    birthday "2017-04-08"
+    phone "MyString"
+    grade "MyString"
+    image "MyString"
+  end
+end
