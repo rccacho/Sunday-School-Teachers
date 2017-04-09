d64d879fa2e5de94bf8ae6a99ce9826938f15d85
rccacho <rccacho@gmail.com>
2017-04-08 21:38:33 -0700

    add root route and sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root          to: 'users#home'
+
   resources :users do
   	resources :messages do
   	  resources :activities, shallow: true
   	end
   end
+
+  get     '/login',    to: 'sessions#new'
+  post    '/login',    to: 'sessions#create'
+  delete  '/logout',   to: 'sessions#destroy'
 end
