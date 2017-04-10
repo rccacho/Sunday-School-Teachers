990ae8d2744ed05387a6462bf590968703035958
rccacho <rccacho@gmail.com>
2017-04-10 12:08:19 -0700

    changes route for messages#index

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   root          to: 'users#home'
 
   resources :users do
-  	resources :messages do
+  	resources :messages, shallow: true, except: (:index) do
   	  resources :activities, shallow: true
   	end
   end
+
+  get '/messages', to: 'messages#index'
 
   get     '/login',    to: 'sessions#new'
   post    '/login',    to: 'sessions#create'
