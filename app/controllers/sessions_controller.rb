80b51ddda20c76bfc8553fecc476a25bd5a7499b
rccacho <rccacho@gmail.com>
2017-04-12 09:38:10 -0700

    change redirect_to from user to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user_path
+      redirect_to root_url
       flash[:success] = "Welcome back, #{user.first_name}!"
     else
       flash.now[:danger] = 'Invalid email/password combination' # Displays flash messages on rendered pages
