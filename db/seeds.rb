ddc1499a681d9fc60584dcdca90a2aa448f55553
rccacho <rccacho@gmail.com>
2017-04-13 10:28:24 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,11 +48,11 @@ potato = User.create({
 })
 
 m1 = Message.create({
-	series: "Metamorphosis",
+	series: "Easter Resurrection!",
 	title: "Week 1",
 	content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	costumes: "antennae",
-	props: "none",
+	costumes: "tunics, Roman soldier costume",
+	props: "basket, sword, rope",
 	featured: "no",
 	user_id: foo.id
 })
